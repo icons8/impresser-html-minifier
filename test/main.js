4c26187f170f5456dc04b5e62aab2be86ded9764
Viacheslav Bereza <betula.icons8@gmail.com>
2015-04-21 09:57:08 +0300

    Migrate to minimize

--- test/main.js
+++ test/main.js
@@ -4,10 +4,28 @@ var
   ;
 
 describe('impress-html-minify', function () {
-  it('should work', function() {
+  it('should work', function(done) {
     var
       minify = new HtmlMinify();
 
-    should(minify.apply('<input type=" text " name="text" required="required" />')).equal('<input name=text required/>');
+    minify.apply('<input type=" text " name="text" required="required" />', function(err, data) {
+      if (err) return done(err);
+
+      should(data).equal('<input type=text name=text required>');
+      done();
+    });
   });
+
+  //it('svg should work', function(done) {
+  //  var
+  //    minify = new HtmlMinify();
+  //
+  //  minify.apply('<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 26 26"><g></g></svg>', function(err, data) {
+  //    if (err) return done(err);
+  //
+  //    should(data).equal('<svg xmlns=http://www.w3.org/2000/svg width=26 height=26 viewbox=\"0 0 26 26\"><g></g></svg>');
+  //    done();
+  //  });
+  //});
+
 });
